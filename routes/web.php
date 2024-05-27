94e3e2cede4c4fbe608aeaba84e4353c91e885a9
Majda <mosseddekmajda@gmail.com>
2024-05-28 01:14:26 +0200

    edit

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PdfController;
+use App\Http\Controllers\RoomController;
+
 
 Route::get('/generate-pdf', [PdfController::class, 'generatePdf']);
 
